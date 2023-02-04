b75f8039936af3441fd753264f2ce394199bbbdc
pablouix <pablouix@gmail.com>
2023-02-04 16:37:02 -0400

    Calculadora UI

---
+++ CalculadoraApp/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿namespace CalculadoraApp;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void btnAC(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnMasMenos(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnPorcentaje(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnDivicion(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnSiete(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnOcho(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnNueve(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnMultiplicar(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnCuatro(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnCinco(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnSeis(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnMenos(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnUno(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnDos(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnTres(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnSuma(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnCero(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnPunto(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnIgual(object sender, EventArgs e)
+    {
+
+    }
+}
+
---
+++ CalculadoraApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CalculadoraApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
